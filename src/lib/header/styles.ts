1c78d3b3bddba16ac7bbe244c225a0ab5bc5486a
gmobrice <gmobrice@outlook.com>
2022-07-02 20:00:47 +0100

    (refactor): improved style functions

--- src/lib/header/styles.ts
+++ src/lib/header/styles.ts
@@ -1,35 +1,31 @@
-const defaultHeaderStyle =
-	'bg-slate-800 bg-opacity-95 backdrop-blur-sm backdrop-saturate-200 drop-shadow-sm duration-200';
-const fullSizedHeaderStyle = `${defaultHeaderStyle} pt-8 pb-6 border-b-2 border-slate-900`;
-const smallerHeaderStyle = `${defaultHeaderStyle} pt-3 pb-3`;
+export const getHeaderStyle = (isSmallerSize: boolean) => `
+  bg-slate-800 
+  bg-opacity-95 
+  backdrop-blur-sm 
+  backdrop-saturate-200 
+  drop-shadow-sm 
+  duration-200
+  ${isSmallerSize ? 'pt-3 pb-3' : 'pt-8 pb-6 border-b-2 border-slate-900'}
+`;
 
-const defaultImageStyle = 'rounded-full';
-const fullSizedImageStyle = `${defaultImageStyle} w-14 h-14`;
-const smallerImageStyle = `${defaultImageStyle} w-8 h-8`;
+export const getImageStyle = (isSmallerSize: boolean) => `
+  rounded-full
+  ${isSmallerSize ? 'w-8 h-8' : 'w-14 h-14'}
+`;
 
-const defaultNameContainerStyle = 'flex ml-4';
-const fullSizedNameContainerStyle = `${defaultNameContainerStyle} flex-col`;
-const smallerNameContainerStyle = `${defaultNameContainerStyle} flex-row items-center`;
+export const getNameContainerStyle = (isSmallerSize: boolean) => `
+  flex
+  ml-4
+  ${isSmallerSize ? 'flex-row items-center' : 'flex-col'}
+`;
 
-const defaultNameStyle = 'text-slate-50 font-semibold';
-const fullSizedNameStyle = `${defaultNameStyle} text-md`;
-const smallerNameStyle = `${defaultNameStyle} text-sm`;
+export const getNameStyle = (isSmallerSize: boolean) => `
+  text-slate-50
+  font-semibold
+  ${isSmallerSize ? 'text-sm' : 'text-md'}
+`;
 
-const defaultSubtitleStyle = 'text-slate-300';
-const fullSizedSubtitleStyle = `${defaultSubtitleStyle} text-sm`;
-const smallerSubtitleStyle = `${defaultSubtitleStyle} text-xs ml-2`;
-
-export const getHeaderStyle = (isSmallerSize: boolean) =>
-	isSmallerSize ? smallerHeaderStyle : fullSizedHeaderStyle;
-
-export const getImageStyle = (isSmallerSize: boolean) =>
-	isSmallerSize ? smallerImageStyle : fullSizedImageStyle;
-
-export const getNameContainerStyle = (isSmallerSize: boolean) =>
-	isSmallerSize ? smallerNameContainerStyle : fullSizedNameContainerStyle;
-
-export const getNameStyle = (isSmallerSize: boolean) =>
-	isSmallerSize ? smallerNameStyle : fullSizedNameStyle;
-
-export const getSubtitleStyle = (isSmallerSize: boolean) =>
-	isSmallerSize ? smallerSubtitleStyle : fullSizedSubtitleStyle;
+export const getSubtitleStyle = (isSmallerSize: boolean) => `
+  text-slate-300
+  ${isSmallerSize ? 'text-xs ml-2' : 'text-sm'}
+`;
--- src/lib/nav/styles.ts
+++ src/lib/nav/styles.ts
@@ -1,20 +1,21 @@
-const defaultLinkStyle = 'text-slate-50 hover:text-sky-300 duration-100 hover:ease-in';
-const fullSizedLinkStyle = `${defaultLinkStyle} text-md`;
-const smallerLinkStyle = `${defaultLinkStyle} text-sm`;
+export const getLinkStyle = (isSmallerSize: boolean, active: string, current: string) => `
+  text-slate-50
+  duration-100
+  hover:text-sky-300
+  hover:ease-in
+  ${isSmallerSize ? 'text-sm' : 'text-md'}
+  ${active === current ? 'font-semibold text-sky-400 hover:text-sky-400' : ''}
+`;
 
-const defaultButtonStyle =
-	'rounded-full bg-rose-500 text-slate-50 text-sm duration-100 hover:bg-rose-600 hover:ease-in active:bg-rose-800 active:ease-in';
-const fullSizedButtonStyle = `${defaultButtonStyle} py-3 px-6`;
-const smallerButtonStyle = `${defaultButtonStyle} py-1 px-4`;
-
-export const getLinkStyle = (isSmallerSize: boolean, active: string, current: string) => {
-	let currentStyles = isSmallerSize ? smallerLinkStyle : fullSizedLinkStyle;
-	if (active === current) {
-		currentStyles += ' font-semibold text-sky-400 hover:text-sky-400';
-	}
-
-	return currentStyles;
-};
-
-export const getButtonStyle = (isSmallerSize: boolean) =>
-	isSmallerSize ? smallerButtonStyle : fullSizedButtonStyle;
+export const getButtonStyle = (isSmallerSize: boolean) => `
+  rounded-full 
+  bg-rose-500 
+  text-slate-50 
+  text-sm 
+  duration-100 
+  hover:bg-rose-600 
+  hover:ease-in 
+  active:bg-rose-800 
+  active:ease-in
+  ${isSmallerSize ? 'py-2 px-4' : 'py-3 px-6'}
+`;
