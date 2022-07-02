388b6c6701ea6f88dcc7f43648d5c86b4abadff1
gmobrice <gmobrice@outlook.com>
2022-07-02 13:03:11 +0100

    (feat): added resizible header on scroll

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,20 +1,47 @@
 <script lang="ts">
+	import { beforeUpdate } from 'svelte';
 	import Nav from '$lib/nav/Nav.svelte';
 	import ProfilePic from '$lib/assets/images/profile-pic-200.jpeg';
+
+	let isSmallerSize: boolean = false;
+	let scrollPosition = 0;
+	const scrollThreshold = 60;
+
+	const defaultHeaderStyle =
+		'bg-slate-800 bg-opacity-95 backdrop-blur-sm backdrop-saturate-200 drop-shadow-sm duration-200';
+
+	const fullSizedHeaderStyle = `${defaultHeaderStyle} pt-8 pb-6 border-b-2 border-slate-900`;
+	const smallerHeaderStyle = `${defaultHeaderStyle} pt-3 pb-3`;
+
+	beforeUpdate(() => {
+		isSmallerSize = scrollPosition >= scrollThreshold;
+	});
 </script>
 
-<header
-	class="sticky top-0 pt-8 pb-6 mb-10 bg-slate-800 bg-opacity-95 backdrop-blur-sm backdrop-saturate-200 border-b-2 border-slate-900 drop-shadow-sm"
->
-	<div class="container flex items-center justify-between">
-		<a href="/" class="flex justify-center items-center">
-			<img class="w-14 h-14 rounded-full" src={ProfilePic} alt="Gustavo Mobrice" />
-			<span class="flex flex-col ml-4">
-				<span class="text-slate-50 text-xl font-semibold">Gustavo Mobrice</span>
-				<span class="text-slate-300 text-sm">software engineer @ microsoft ireland</span>
-			</span>
-		</a>
-		<Nav />
+<svelte:window bind:scrollY={scrollPosition} />
+
+<header class="sticky top-0 h-32 mb-10">
+	<div class={isSmallerSize ? smallerHeaderStyle : fullSizedHeaderStyle}>
+		<div class="container flex items-center justify-between">
+			{#if isSmallerSize}
+				<a href="/" class="flex justify-center items-center">
+					<img class="w-8 h-8 rounded-full" src={ProfilePic} alt="Gustavo Mobrice" />
+					<span class="flex items-center ml-4">
+						<span class="text-slate-50 text-sm font-semibold">Gustavo Mobrice</span>
+						<span class="text-slate-300 text-xs ml-2">software engineer @ microsoft ireland</span>
+					</span>
+				</a>
+			{:else}
+				<a href="/" class="flex justify-center items-center">
+					<img class="w-14 h-14 rounded-full" src={ProfilePic} alt="Gustavo Mobrice" />
+					<span class="flex flex-col ml-4">
+						<span class="text-slate-50 text-xl font-semibold">Gustavo Mobrice</span>
+						<span class="text-slate-300 text-sm">software engineer @ microsoft ireland</span>
+					</span>
+				</a>
+			{/if}
+			<Nav isSmallerNav={isSmallerSize} />
+		</div>
 	</div>
 </header>
 
--- src/lib/nav/Nav.svelte
+++ src/lib/nav/Nav.svelte
@@ -3,6 +3,8 @@
 	import { beforeUpdate } from 'svelte';
 	import { navigationLinks } from './data';
 
+	export let isSmallerNav: boolean;
+
 	let activePath = $page.url.pathname;
 	const linkStyle = 'text-slate-50 hover:text-sky-300 duration-100 hover:ease-in';
 	const mailEmoji = '✉️';
@@ -13,30 +15,31 @@
 </script>
 
 <nav>
-	<ul class="flex space-x-6">
-		{#each navigationLinks as { id, name, href } (id)}
+	{#if isSmallerNav}
+		<span class="text-white">small nav</span>
+	{:else}
+		<ul class="flex space-x-6">
+			{#each navigationLinks as { id, name, href } (id)}
+				{@const currentLinkStyle =
+					href === activePath
+						? `${linkStyle} font-semibold text-sky-400 hover:text-sky-400`
+						: linkStyle}
+
+				<li class="list-none">
+					<a class={currentLinkStyle} {href}>
+						{name}
+					</a>
+				</li>
+			{/each}
 			<li class="list-none">
 				<a
-					class={`${linkStyle} ${
-						activePath === href && 'font-semibold text-sky-400 hover:text-sky-400'
-					}`}
-					{href}
+					class="py-3 px-6 rounded-full bg-rose-500 text-slate-50 text-sm duration-100 hover:bg-rose-600 hover:ease-in active:bg-rose-800 active:ease-in"
+					href="mailto:gmobrice@outlook.com"
 				>
-					{name}
+					<span class="mr-1">{mailEmoji}</span>
+					Contact me!
 				</a>
 			</li>
-		{/each}
-		<li class="list-none">
-			<a
-				class="py-3 px-6 rounded-full bg-rose-500 text-slate-50 text-sm duration-100 hover:bg-rose-600 hover:ease-in active:bg-rose-800"
-				href="mailto:gmobrice@outlook.com"
-			>
-				<span class="mr-1">{mailEmoji}</span>
-				Contact me!
-			</a>
-		</li>
-	</ul>
+		</ul>
+	{/if}
 </nav>
-
-<style lang="scss">
-</style>
